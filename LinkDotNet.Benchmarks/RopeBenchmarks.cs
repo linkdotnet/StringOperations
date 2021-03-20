e93256f4e35847d78244ddd7971fba2ce40ef234
Steven Giesel <stgiesel35@gmail.com>
2021-03-20 16:13:22 +0100

    Added Benchmark Project

---
+++ LinkDotNet.Benchmarks/RopeBenchmarks.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LinkDotNet.Benchmarks
+{
+    internal class RopeBenchmarks
+    {
+        internal static void Main()
+        {
+            BenchmarkRunner.Run(typeof(RopeConcatTests).Assembly);
+        }
+    }
+}
